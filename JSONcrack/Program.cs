2b707f6f0fbf6417edda083edebcea78898bf42b
bakilv <bakilv@310-16>
2026-02-16 09:33:45 +0100

    Elso feladat

--- JSONcrack/Program.cs
+++ JSONcrack/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace JSONcrack
+﻿using System.Diagnostics.Tracing;
+using System.Text.Json;
+
+namespace JSONcrack
 {
+    class Adat
+    {
+        public List<string> nevek { get; set; }
+        public List<int> korok { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String fajl = File.ReadAllText("C:\\Users\\bakilv\\source\\repos\\JSONcrack\\JSONcrack\\adatok.json", System.Text.Encoding.Latin1);
+            Adat adat = JsonSerializer.Deserialize<Adat>(fajl);
+
+
+            foreach (var nev in adat.nevek)
+            {
+                Console.WriteLine(nev);
+            }
+            Console.WriteLine($"{adat.nevek[0]} életkor:{adat.korok[0]}");
         }
     }
 }
