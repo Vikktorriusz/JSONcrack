649fd74fa5b9733c5c7ad78975a8ce5f811a6ded
bakilv <bakilv@310-16>
2026-02-16 10:16:05 +0100

    2.feladat kesz

--- JSONcrack/Program.cs
+++ JSONcrack/Program.cs
@@ -8,6 +8,11 @@ namespace JSONcrack
         public List<string> nevek { get; set; }
         public List<int> korok { get; set; }
     }
+    class Diak
+    {
+        public string nev { get; set; }
+        public List<int> jegyek { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -16,11 +21,29 @@ namespace JSONcrack
             Adat adat = JsonSerializer.Deserialize<Adat>(fajl);
 
 
-            foreach (var nev in adat.nevek)
+            foreach (var nev1 in adat.nevek)
             {
-                Console.WriteLine(nev);
+                Console.WriteLine(nev1);
             }
             Console.WriteLine($"{adat.nevek[0]} életkor:{adat.korok[0]}");
+            String fajl2 = File.ReadAllText("C:\\Users\\bakilv\\source\\repos\\JSONcrack\\JSONcrack\\diakok.json", System.Text.Encoding.Latin1);
+            Console.WriteLine(fajl2);
+            List<Diak> diakok = JsonSerializer.Deserialize<List<Diak>>(fajl2);
+            bool megvan = false;
+            Console.Write("Keresett név:");
+            String nev = Console.ReadLine();
+            foreach (var diak in diakok)
+            {
+                if (diak.nev == nev)
+                {
+                    Console.WriteLine("Átlaga" + diak.jegyek.Average());
+                    megvan = true;
+                }
+            }
+            if(megvan==false)
+            {
+                Console.WriteLine("Nincs ilyen nevű diák");
+            }
         }
     }
 }
